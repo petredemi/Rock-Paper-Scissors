258db9903cd9a35725d46d2ceca0952a99f3bf4a
petrudemian <petredemi@gmail.com>
2023-03-14 14:29:27 +0000

    created loop and score

--- java-script.js
+++ java-script.js
@@ -4,40 +4,59 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return wordPick[randomNumber];
 }
-getComputerChoice();
 
 function playRound(playerSelection, computerSelection){
    
     if (playerSelection === 'rock' & computerSelection === 'paper'){
-        return 'You lose, Paper beats Rock';
+        return 'You lose!';
 
     } else if ( playerSelection === 'rock' & computerSelection === 'scissors'){
-        return 'You Win! Rock beat Scissor';
+        return 'You Win!';
     }
     else if ( playerSelection === 'paper' & computerSelection === 'scissors'){
-        return 'You lose, Scissors beat paper';
+        return 'You lose!';
 
     } else if (playerSelection === 'paper' & computerSelection ==='rock'){
-        return 'You Win! Paper beat Rock';
+        return 'You Win!';
     } 
     else if ( playerSelection === 'scissors' & computerSelection === 'rock'){
-        return 'You lose, Rock beat scissors';
+        return 'You lose!';
     
     } else if ( playerSelection === 'scissors' & computerSelection === 'paper'){
-        return 'You Win! Scissor beat Paper';
+        return 'You Win!';
     } else (playerSelection === computerSelection) 
-        return "You are equal, play again";
+        return 'Equal!';
 }
-const playerSelection = 'rock';
-const computerSelection = getComputerChoice();
-console.log('Computer: ' + computerSelection);
-console.log('You: ' + playerSelection);
 
-console.log(playRound(playerSelection, computerSelection ));
+function whoWin(roundWinner){
 
-function game() {
-    playRound(){
-        
+    if (roundWinner === 'Equal!'){
+        computerScore += 0;
+    }
+    else if(roundWinner === 'You Win!'){
+        yourScore += 1;
+    }else if (roundWinner === 'You lose!'){
+        computerScore += 1;
     }
 }
+
+let yourScore = 0;
+let computerScore = 0;
+
+function game(){
+for (let i = 1 ; i < 4; i++){
+    const playerSelection = prompt('enter rock paper or scissors').toLowerCase();
+    const computerSelection = getComputerChoice();
+    roundWinner = playRound(playerSelection, computerSelection);
+
+    console.log(`You choose: ${playerSelection}`);
+    console.log(`Computer Choose: ${computerSelection}`);
+    console.log(i + ' ' + playRound(playerSelection, computerSelection));
+    console.log(whoWin(roundWinner));
+    
+    }
+}
+game();
+console.log(`Computer: ${computerScore}`);
+console.log(`Player: ${yourScore}`);
 
